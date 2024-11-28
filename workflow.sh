a726b75689498454dc019614ee2fe0b8f15b8865
Rizky Jayusman <rizky.jayusman96@gmail.com>
2024-11-28 15:31:46 +0700

    fix: create branch based on main or development branch

--- workflow.sh
+++ workflow.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+MAIN_BRANCH="main"
+DEVELOPMENT_BRANCH="development"
+
 if [ "$#" -lt 3 ]; then
-  echo "Usage: sh gitworkflow.sh <branch|commit> <type> <branch-name|message>"
-  echo "Allowed types for branch: feature, bugfix, hotfix"
-  echo "Allowed types for commit: feat, fix, refactor, chore"
+  echo "Usage: sh gitworkflow.sh <branch|commit> <type> <branch-name|scope branch-name>"
+  echo "Allowed branch types: feature, bugfix, hotfix"
+  echo "Allowed commit types: feat, fix, refactor, chore"
   exit 1
 fi
 
@@ -12,28 +15,41 @@ TYPE=$2
 
 if [ "$ACTION" == "branch" ]; then
   if [ "$#" -eq 3 ]; then
-    BRANCH_NAME="$TYPE/${@:3}"  # No scope, just type and branch name
+    BRANCH_NAME="$TYPE/${@:3}" # No scope
   else
-    BRANCH_NAME="$TYPE/${@:3:1}/${@:4}"  # With scope, type, scope, and branch name
+    BRANCH_NAME="$TYPE/${@:3:1}/${@:4}" # With scope
   fi
-  
+
+  BASE_BRANCH=""
+  if [ "$TYPE" == "feature" ]; then
+    BASE_BRANCH=$DEVELOPMENT_BRANCH
+  elif [ "$TYPE" == "bugfix" ]; then
+    BASE_BRANCH=$DEVELOPMENT_BRANCH
+  elif [ "$TYPE" == "hotfix" ]; then
+    BASE_BRANCH=$MAIN_BRANCH
+  else
+    echo "ERROR: Invalid branch type. Allowed types: feature, bugfix, hotfix."
+    exit 1
+  fi
+
   PATTERN="^(feature|bugfix|hotfix)(\/[a-zA-Z0-9\-]+)?\/[a-zA-Z0-9\-]+$"
-  
   if [[ ! "$BRANCH_NAME" =~ $PATTERN ]]; then
     echo "ERROR: Invalid branch name format."
     echo "Branch name must match the format: <type>/<scope>/<branch-name> or <type>/<branch-name>"
-    echo "Allowed types: feature, bugfix, hotfix"
     exit 1
   fi
-  
+
   if git show-ref --quiet refs/heads/"$BRANCH_NAME"; then
     echo "ERROR: Branch '$BRANCH_NAME' already exists."
     exit 1
   fi
 
+  git checkout "$BASE_BRANCH" || exit 1
+  git pull origin "$BASE_BRANCH" || exit 1
+
   git checkout -b "$BRANCH_NAME"
-  echo "Switched to new branch '$BRANCH_NAME'"
-  
+  echo "Switched to new branch '$BRANCH_NAME' from '$BASE_BRANCH'"
+
 elif [ "$ACTION" == "commit" ]; then
   MESSAGE="${@:3}"
 
