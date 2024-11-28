868349932a8f0894e81459d9a08a267d1ca7aa72
Rizky Jayusman <rizky.jayusman96@gmail.com>
2024-11-28 14:57:58 +0700

    feat: add branch naming checker using pre-push hooks

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 echo "Installing hooks..."
 
 cp .github/hooks/commit-msg .git/hooks/commit-msg
+cp .github/hooks/pre-push .git/hooks/pre-push
+
 chmod +x .git/hooks/commit-msg
+chmod +x .git/hooks/pre-push
 
 echo "Completed."
